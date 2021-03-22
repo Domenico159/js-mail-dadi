7b025ad34b10eccdeaccb741743dfa61e8d55482
Domenico159 <domenico.zummo@gmail.com>
2021-03-22 15:29:04 +0100

    Project 1 and 2 completed

--- js/script.js
+++ js/script.js
@@ -10,11 +10,11 @@ function mail() {
 
     if(isNaN(user)){
         for (var i = 0; i < countMax; i++){
-            console.log(countMax);
+            // console.log(countMax);
     
             if((allUser[i] === user) && (isNaN)) {
                 userFound=true;
-                console.log('trovato');
+                // console.log('trovato');
             }
         }
     
@@ -24,6 +24,23 @@ function mail() {
             document.getElementById('result').innerHTML = 'Account non trovato o non corretto , per favore riprovare.';
         }
     }else{
-        alert('Attenzione , caratteri non corretti , per vafore inserire un formato letterario, grazie.');
+        alert('Attenzione , caratteri non corretti o inesistenti , per vafore inserire un formato letterario, grazie.');
     }
+}
+
+
+// Esercizio numero 2
+
+function gioco() {
+    var user1 = document.getElementById("user1").innerHTML = Math.floor(Math.random() * 6) + 1;
+    var computer = document.getElementById("computer").innerHTML = Math.floor(Math.random() * 6) + 1;
+
+    if(user1 > computer){
+        document.getElementById('risultato').innerHTML = 'HA VINTO IL GIOCATORE USER1';
+    }else if (user1 < computer) {
+        document.getElementById('risultato').innerHTML = 'HA VINTO IL COMPUTER';
+    }else{
+        document.getElementById('risultato').innerHTML = 'PARTITA FINITA CON UN PAREGGIO';
+    }
+
 }
